e614b5c8690dce8eb39b5e887cf9ad9152a1ee8f
tpyoung <tyleryoung@Tylers-MacBook-Pro.local>
2016-03-08 10:45:57 -1000

    finished project, included original attempts

--- multiples.js
+++ multiples.js
@@ -3,10 +3,19 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
+
+
+
+ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
-
-  // do your work here
-
+  for (i = 1; i < n; i++){
+    if (i % 3 === 0 || i % 5 === 0) {
+      // arr2.push(i);
+      sum += i;
+  }
+}
   return sum;
 };
+
+ // var arr = [0,1,2,3,4,5,6,7,8,9,10];
+  // var arr2 =[0];
